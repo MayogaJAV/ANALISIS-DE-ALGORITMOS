3e8dc27f571e9e7050ca80ba3af9d04a14950d08
MayogaJAV <104171954+MayogaJAV@users.noreply.github.com>
2023-07-05 10:17:54 -0500

    Update Problema del Cambio de Monedas.cpp

--- Analisis de Algoritmos/4. Algoritmo Voraz/Problema del Cambio de Monedas.cpp
+++ Analisis de Algoritmos/4. Algoritmo Voraz/Problema del Cambio de Monedas.cpp
@@ -3,33 +3,33 @@
 #include <vector>
 using namespace std;
 
-vector<int> cambioMonedas(vector<int> monedas, int cantidad) {
-	vector<int> cambio;
-	int i = monedas.size() - 1;
-	
-	while (cantidad > 0 && i >= 0) {
-		if (monedas[i] <= cantidad) {
-			cambio.push_back(monedas[i]);
-			cantidad -= monedas[i];
-		} else {
-			i--;
-		}
-	}
-	
-	return cambio;
+vector<int> cambioMonedas(const vector<int>& monedas, unsigned int cantidad) {
+    vector<int> cambio;
+    int i = monedas.size() - 1;
+
+    while (cantidad > 0 && i >= 0) {
+        if (monedas[i] <= cantidad) {
+            cambio.push_back(monedas[i]);
+            cantidad -= monedas[i];
+        } else {
+            i--;
+        }
+    }
+
+    return cambio;
 }
 
 int main() {
-	vector<int> monedas = {1, 2, 5, 10, 20, 50, 100, 200, 500};
-	int cantidad = 123;
-	
-	vector<int> cambio = cambioMonedas(monedas, cantidad);
-	
-	cout << "Cambio de monedas: ";
-	for (int i = 0; i < cambio.size(); i++) {
-		cout << cambio[i] << " ";
-	}
-	cout << endl;
-	
-	return 0;
+    vector<int> monedas = {1, 2, 5, 10, 20, 50, 100, 200, 500};
+    unsigned int cantidad = 123;
+
+    vector<int> cambio = cambioMonedas(monedas, cantidad);
+
+    cout << "Cambio de monedas: ";
+    for (auto it = cambio.begin(); it != cambio.end(); ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
