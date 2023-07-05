aa97c5e0978907e3d1ac017a5e75ed9e95dcddac
MayogaJAV <104171954+MayogaJAV@users.noreply.github.com>
2023-07-05 10:24:43 -0500

    Update Quick Sort.cpp

--- Analisis de Algoritmos/3. Divide y Venceras/Quick Sort.cpp
+++ Analisis de Algoritmos/3. Divide y Venceras/Quick Sort.cpp
@@ -1,30 +1,36 @@
 //Quick Sort
 #include<iostream>
 using namespace std;
-void swap(int* a, int* b) {
-	int t = *a;
-	*a = *b;
-	*b = t;
+
+void swap(int& a, int& b) {
+	int t = a;
+	a = b;
+	b = t;
 }
 
 int partition(int arr[], int low, int high) {
 	int pivot = arr[high];
 	int i = (low - 1);
 	
-	for (int j = low; j <= high - 1; j++) {
-		if (arr[j] <= pivot) {
+	// Colocar todos los elementos menores que el pivote al principio y los mayores al final
+	for (int *j = &arr[low]; j <= &arr[high - 1]; j++) {
+		if (*j <= pivot) {
 			i++;
-			swap(&arr[i], &arr[j]);
+			swap(arr[i], *j);
 		}
 	}
 	
-	swap(&arr[i + 1], &arr[high]);
+	// Colocar el pivote en su posición correcta
+	swap(arr[i + 1], arr[high]);
 	return (i + 1);
 }
 
 void quickSort(int arr[], int low, int high) {
 	if (low < high) {
+		// Obtener la posición del pivote
 		int pi = partition(arr, low, high);
+		
+		// Ordenar recursivamente los elementos antes y después del pivote
 		quickSort(arr, low, pi - 1);
 		quickSort(arr, pi + 1, high);
 	}
@@ -37,9 +43,10 @@ int main() {
 	quickSort(arr, 0, n - 1);
 	
 	cout << "Arreglo ordenado: \n";
-	for (int i = 0; i < n; i++)
-		cout << arr[i] << " ";
+	for (int *i = arr; i < arr + n; i++)
+		cout << *i << " ";
 	cout << endl;
 	
 	return 0;
 }
+
