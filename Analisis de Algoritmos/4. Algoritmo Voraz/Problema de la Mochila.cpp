011e8126bc3e6dc5049c8a76bfb79ecdb3548b46
MayogaJAV <104171954+MayogaJAV@users.noreply.github.com>
2023-07-05 09:13:31 -0500

    Update Problema de la Mochila.cpp

--- Analisis de Algoritmos/4. Algoritmo Voraz/Problema de la Mochila.cpp
+++ Analisis de Algoritmos/4. Algoritmo Voraz/Problema de la Mochila.cpp
@@ -5,43 +5,44 @@
 using namespace std;
 
 struct Objeto {
-	int peso;
-	int valor;
+    double peso;
+    double valor;
 };
 
-bool compararPorRatio(Objeto o1, Objeto o2) {
-	double ratio1 = (double)o1.valor / o1.peso;
-	double ratio2 = (double)o2.valor / o2.peso;
-	return ratio1 > ratio2;
+bool compararPorRatio(const Objeto& o1, const Objeto& o2) {
+    double ratio1 = o1.valor / o1.peso;
+    double ratio2 = o2.valor / o2.peso;
+    return ratio1 > ratio2;
 }
 
-int problemaMochila(vector<Objeto> objetos, int capacidad) {
-	sort(objetos.begin(), objetos.end(), compararPorRatio);
-	
-	int pesoActual = 0;
-	int valorTotal = 0;
-	
-	for (int i = 0; i < objetos.size(); i++) {
-		if (pesoActual + objetos[i].peso <= capacidad) {
-			pesoActual += objetos[i].peso;
-			valorTotal += objetos[i].valor;
-		} else {
-			int espacioRestante = capacidad - pesoActual;
-			valorTotal += (objetos[i].valor / objetos[i].peso) * espacioRestante;
-			break;
-		}
-	}
-	
-	return valorTotal;
+double problemaMochila(const vector<Objeto>& objetos, double capacidad) {
+    vector<Objeto> objetosOrdenados(objetos);
+    sort(objetosOrdenados.begin(), objetosOrdenados.end(), compararPorRatio);
+
+    double pesoActual = 0;
+    double valorTotal = 0;
+
+    for (auto it = objetosOrdenados.begin(); it != objetosOrdenados.end(); ++it) {
+        if (pesoActual + it->peso <= capacidad) {
+            pesoActual += it->peso;
+            valorTotal += it->valor;
+        } else {
+            double espacioRestante = capacidad - pesoActual;
+            valorTotal += (it->valor / it->peso) * espacioRestante;
+            break;
+        }
+    }
+
+    return valorTotal;
 }
 
 int main() {
-	vector<Objeto> objetos = {{2, 10}, {3, 5}, {5, 15}, {7, 7}, {1, 6}, {4, 18}, {1, 3}};
-	int capacidadMochila = 15;
-	
-	int valorMaximo = problemaMochila(objetos, capacidadMochila);
-	
-	cout << "Valor maximo en la mochila: " << valorMaximo << endl;
-	
-	return 0;
+    vector<Objeto> objetos = {{2, 10}, {3, 5}, {5, 15}, {7, 7}, {1, 6}, {4, 18}, {1, 3}};
+    double capacidadMochila = 15;
+
+    double valorMaximo = problemaMochila(objetos, capacidadMochila);
+
+    cout << "Valor maximo en la mochila: " << valorMaximo << endl;
+
+    return 0;
 }
