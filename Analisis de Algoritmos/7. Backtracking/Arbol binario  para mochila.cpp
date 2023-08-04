d731acbe7decec1dee766722ccce87be03675c51
MayogaJAV <104171954+MayogaJAV@users.noreply.github.com>
2023-08-03 21:05:33 -0500

    Add files via upload

---
+++ Analisis de Algoritmos/7. Backtracking/Arbol binario  para mochila.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Objeto {
+	int peso;
+	int valor;
+};
+
+int mochilaBinaria(vector<Objeto>& objetos, int capacidad) {
+	int n = objetos.size();
+	vector<vector<int>> dp(n + 1, vector<int>(capacidad + 1, 0));
+	
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= capacidad; j++) {
+			if (objetos[i - 1].peso <= j) {
+				dp[i][j] = max(dp[i - 1][j],
+					dp[i - 1][j - objetos[i - 1].peso] + objetos[i - 1].valor);
+			} else {
+				dp[i][j] = dp[i - 1][j];
+			}
+		}
+	}
+	
+	return dp[n][capacidad];
+}
+
+int main() {
+	// Ejemplo de uso
+	vector<Objeto> objetos = {{2, 12}, {1, 10}, {3, 20}, {2, 15}};
+	int capacidad = 5;
+	
+	int valorMaximo = mochilaBinaria(objetos, capacidad);
+	
+	cout << "Valor máximo de la mochila: " << valorMaximo << endl;
+	
+	return 0;
+}
---
+++ Analisis de Algoritmos/7. Backtracking/Arbol combinatorio para mochila.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Objeto {
+	int peso;
+	int valor;
+};
+
+int mochilaCombinatoria(vector<Objeto>& objetos, int capacidad, int pesoActual, int valorActual, int indice) {
+	if (indice >= objetos.size() || pesoActual >= capacidad) {
+		return valorActual;
+	}
+	
+	int valorMaximo = valorActual;
+	
+	for (int i = indice; i < objetos.size(); i++) {
+		if (pesoActual + objetos[i].peso <= capacidad) {
+			int nuevoValor = mochilaCombinatoria(objetos, capacidad, pesoActual + objetos[i].peso,
+												 valorActual + objetos[i].valor, i + 1);
+			valorMaximo = max(valorMaximo, nuevoValor);
+		}
+	}
+	
+	return valorMaximo;
+}
+
+int main() {
+	// Ejemplo de uso
+	vector<Objeto> objetos = {{2, 12}, {1, 10}, {3, 20}, {2, 15}};
+	int capacidad = 5;
+	
+	int valorMaximo = mochilaCombinatoria(objetos, capacidad, 0, 0, 0);
+	
+	cout << "Valor máximo de la mochila: " << valorMaximo << endl;
+	
+	return 0;
+}
